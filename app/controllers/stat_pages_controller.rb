423279fbd575131257507c335ed28e4c59239a7f
Mzoro <zorkiy2000@gmail.com>
2016-05-31 22:21:03 +0300

    complete stat_pages

---
+++ app/controllers/stat_pages_controller.rb
@@ -0,0 +1,10 @@
+class StatPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'stat_pages#home'
+
+  get 'stat_pages/help'
+
+  get 'stat_pages/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
