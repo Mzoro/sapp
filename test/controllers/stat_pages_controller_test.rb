d97f5efc8b3e924d41569b6c737153e021b56115
Mzoro <zorkiy2000@gmail.com>
2016-06-03 22:16:37 +0300

    remove test gems

--- test/controllers/stat_pages_controller_test.rb
+++ test/controllers/stat_pages_controller_test.rb
@@ -1,22 +1,25 @@
 require 'test_helper'
 
 class StatPagesControllerTest < ActionController::TestCase
+  setup do
+    @same_title = "Ruby on Rails Tutorial Sample App"
+  end  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@same_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@same_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@same_title}"
   end
 
 end
