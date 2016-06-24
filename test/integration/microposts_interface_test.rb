8b2d7f8962e1215936f52ee0f8feffda5deaed9e
Mzoro <zorkiy2000@gmail.com>
2016-06-24 18:54:05 +0300

    complete microposts without cloud_storage

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,15 +9,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select "div.pagination"  
+    assert_select 'input[type=file]'
     assert_no_difference 'Micropost.count' do
       post microposts_path, micropost: { content: "" }
     end
     assert_select 'div#error_explanation'
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
-    assert_redirected_to root_url
+    d = assigns(:micropost)
+    assert d.picture?
     follow_redirect!
     assert_match content, response.body
     assert_select 'a', text: 'delete'
@@ -28,4 +31,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get user_path(users(:goga))
     assert_select 'a', text: "delete", count: 0
   end
+
+  test "microposts sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "34 microposts", response.body
+    #user with 0 microposts
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "gagaga")
+    get root_path
+    assert_match "1 micropost", response.body
+  end  
 end
