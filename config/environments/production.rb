5b2cd6829d65443711deff0d0288c335d0efc4cf
Mzoro <zorkiy2000@gmail.com>
2016-06-26 02:10:06 +0300

    update email_config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,15 +65,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 's-app-zoro.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host}
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address              => 'smtp.sengrid.net',
-    :port                 => '587',
-    :authentication       => :plain,
-    :user_name            => ENV['SENGRID_USERNAME'],
-    :password             => ENV['SENGRID_PASSWORD'],
-    :domain               => 'heroku.com',
-    :enable_starttls_auto => true 
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
   
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,7 +32,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: 'example.com' }
 
-
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
 
